40e1d64552d560f3a826a85089c4177b276d9af6
Simon Pamies <spamsch@gmail.com>
2025-07-13 11:57:14 +0200

    Replace logging macros with printf for motor R/L and flux linkage response processing

--- vesc_can_sdk.c
+++ vesc_can_sdk.c
@@ -354,10 +354,10 @@ static void vesc_process_can_frame_and_store_information(uint32_t controller_id,
     (void)controller_id;
     // Results for motor_r_l and flux_linkage detection should be stored internally
     if (command == COMM_DETECT_MOTOR_R_L) {
-        LOG_INFO("Processing motor R/L response for controller %d", controller_id);
+        printf("Processing motor R/L response for controller %d", controller_id);
         vesc_parse_motor_rl_response(data, len, &sdk_state.motor_rl_response);
     } else if (command == COMM_DETECT_MOTOR_FLUX_LINKAGE) {
-        LOG_INFO("Processing flux linkage response for controller %d", controller_id);
+        printf("Processing flux linkage response for controller %d", controller_id);
         vesc_parse_flux_linkage_response(data, len, &sdk_state.flux_linkage_response);
     } else {
         LOG_WARNING("Received unexpected command %d for controller %d", command, controller_id);
